b8e4161da0cb35e4bd6723d715fd937a9ae709e9
TheGreatPintoJ <161981999+TheGreatPintoJ@users.noreply.github.com>
2025-09-19 12:55:47 +0000

    Add files via upload

---
+++ src/main/java/me/pintoadmin/pintoKits/PintoKits.java
@@ -0,0 +1,56 @@
+package me.pintoadmin.pintoKits;
+
+import org.bukkit.configuration.*;
+import org.bukkit.configuration.file.*;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PintoKits extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        saveKitsConfig();
+
+        new SaveKitCommand(this);
+        new KitCommand(this);
+        new RemoveKitCommand(this);
+        new KitCompleter(this);
+
+        addKitPerms();
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    public ConfigurationSection getKitsSection() {
+        FileConfiguration config = YamlConfiguration.loadConfiguration(this.getDataFolder().toPath().resolve("kits.yml").toFile());
+        if (!config.isConfigurationSection("kits")) {
+            config.createSection("kits");
+            this.saveConfig();
+        }
+        return config.getConfigurationSection("kits");
+    }
+
+    public void saveKitsConfig() {
+        try {
+            FileConfiguration config = YamlConfiguration.loadConfiguration(this.getDataFolder().toPath().resolve("kits.yml").toFile());
+            config.save(this.getDataFolder().toPath().resolve("kits.yml").toFile());
+        } catch (Exception e) {
+            getLogger().severe("Could not save kits.yml: " + e.getMessage());
+        }
+    }
+
+    public void addKitPerms(){
+        if(getServer().getPluginManager().getPermission("pintokits.kit.*") == null)
+            getServer().getPluginManager().addPermission(new org.bukkit.permissions.Permission("pintokits.kit.*"));
+
+        if(getKitsSection() != null) {
+            for (String kitName : getKitsSection().getKeys(false)) {
+                if(getServer().getPluginManager().getPermission("pintokits.kit." + kitName.toLowerCase()) == null)
+                    getServer().getPluginManager().addPermission(new org.bukkit.permissions.Permission("pintokits.kit." + kitName.toLowerCase()));
+            }
+        }
+    }
+}
---
+++ src/main/java/me/pintoadmin/pintoKits/RemoveKitCommand.java
@@ -0,0 +1,54 @@
+package me.pintoadmin.pintoKits;
+
+import org.bukkit.*;
+import org.bukkit.command.*;
+import org.bukkit.configuration.*;
+import org.bukkit.configuration.file.*;
+
+import java.io.*;
+
+import static org.bukkit.Bukkit.getServer;
+
+public class RemoveKitCommand implements CommandExecutor {
+    private final PintoKits plugin;
+
+    public RemoveKitCommand(PintoKits plugin) {
+        this.plugin = plugin;
+        plugin.getCommand("removekit").setExecutor(this);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length == 0) {
+            sender.sendMessage("Usage: /removekit <kitname>");
+            return true;
+        }
+
+        String kitName = args[0].toLowerCase();
+
+        if (plugin.getKitsSection() != null && plugin.getKitsSection().isConfigurationSection(kitName)) {
+            File kitsFile = new File(plugin.getDataFolder(), "kits.yml");
+            FileConfiguration config = YamlConfiguration.loadConfiguration(kitsFile);
+
+            ConfigurationSection kitsSection = plugin.getKitsSection();
+            if (!kitsSection.isConfigurationSection(kitName)) {
+                sender.sendMessage(ChatColor.GREEN+"Kit not found: " + kitName);
+                return true;
+            }
+
+            config.getConfigurationSection("kits").set(kitName, null);
+            try {
+                config.save(kitsFile);
+            } catch (IOException e) {
+                sender.sendMessage("An error occurred while removing the kit.");
+                e.printStackTrace();
+                return true;
+            }
+            sender.sendMessage(ChatColor.GREEN+"Kit removed: " + kitName);
+            getServer().getPluginManager().removePermission("pintokits.kit." + kitName);
+        } else {
+            sender.sendMessage(ChatColor.RED+"Kit not found: " + kitName);
+        }
+        return true;
+    }
+}
